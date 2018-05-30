f861507578ce7ee4c6a2eb35e7cd78f5be20dfe5
DJ <DJ@DJ>
2018-05-31 07:06:55 +0900

    DJ pom org.bgee.log4jdbc-log4j2추가 오류 수정중 나머진 없음

--- src/main/java/com/cm/takemeal/noticeboard/model/dao/BoardDaoTest.java
+++ src/main/java/com/cm/takemeal/noticeboard/model/dao/BoardDaoTest.java
@@ -4,11 +4,20 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.cm.takemeal.noticeboard.model.vo.BoardVo;
 import com.cm.takemeal.noticeboard.model.vo.Criteria;
+
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations= {"file:src/main/resources/**/root-context.xml"})
 
 public class BoardDaoTest {
 
@@ -18,7 +27,17 @@ public class BoardDaoTest {
 	private static Logger logger = LoggerFactory.getLogger(BoardDaoTest.class);
 	
 	
-	public void testListCriterai()throws Exception{
+	@Test
+	public void testCreate()throws Exception{
+		
+		BoardVo board = new BoardVo();
+		board.setTitle("새로운글 테스트중");
+		board.setContent("새로운 글 테스트중");
+		board.setWritet("DJ");
+		dao.create(board);
+	}
+	
+	/*public void testListCriterai()throws Exception{
 	
 		Criteria cri = new Criteria();
 		cri.setPage(2);
@@ -28,7 +47,7 @@ public class BoardDaoTest {
 		
 		for(BoardVo boardVo : list) {
 			logger.info(boardVo.getBno() + ":" + boardVo.getTitle());
-		}
+		}*/
 		
 
 		
@@ -38,4 +57,3 @@ public class BoardDaoTest {
 	
 	
 
-}
