28a12aeba29c6896b1e3733d9c98b2192bdcd39c
DJ <DJ@DJ>
2018-06-06 12:35:18 +0900

    DJ 작업중...

--- src/main/java/com/cm/takemeal/noticeboard/controller/SearchBoardController.java
+++ src/main/java/com/cm/takemeal/noticeboard/controller/SearchBoardController.java
@@ -9,9 +9,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.cm.takemeal.noticeboard.model.service.BoardService;
+import com.cm.takemeal.noticeboard.model.vo.BoardVo;
 import com.cm.takemeal.noticeboard.model.vo.PageMaker;
 import com.cm.takemeal.noticeboard.model.vo.SearchCriteria;
 
@@ -42,6 +45,50 @@ public class SearchBoardController {
 		
 	}
 	
+	@RequestMapping(value="/reaPage", method=RequestMethod.GET)
+	public ModelAndView read(@RequestParam("bno")int bno, @ModelAttribute("cri")SearchCriteria cri, ModelAndView mv)
+			throws Exception{
+		
+		/*model.addAttribute(service.read(bno));*/
+		mv.addObject(service.read(bno));
+		mv.setViewName("dj/sboard/read");
+		return mv;
+	}
 	
+	@RequestMapping(value="/removePage", method=RequestMethod.POST)
+	public String remove(@RequestParam("bno")int bno, SearchCriteria cri, RedirectAttributes rttr)throws Exception{
+		
+		service.remove(bno);
+		rttr.addAttribute("page", cri.getPage());
+		rttr.addAttribute("perPageNum", cri.getPerPageNum());
+		rttr.addAttribute("searchType", cri.getSearchType());
+		rttr.addAttribute("keyword", cri.getKeyword());
+		
+		rttr.addFlashAttribute("msg","SUCCESS");
+		
+		return "redirect:/sboard/list";
+	
+	}
+	
+	@RequestMapping(value="/modifyPage.do", method=RequestMethod.GET)
+	public void modifyPagingGET(int bno, @ModelAttribute("cri")SearchCriteria cri, Model model)throws Exception{
+		model.addAttribute(service.read(bno));
+	} 
+	
+	
+	@RequestMapping(value="/modifyPage.do", method=RequestMethod.POST)
+	public String modifyPagingPOST(BoardVo board, SearchCriteria cri, RedirectAttributes rttr)throws Exception{
+		logger.info(cri.toString());
+		service.modify(board);
+		
+		rttr.addAttribute("page", cri.getPage());
+		rttr.addAttribute("perPageNum", cri.getPerPageNum());
+		rttr.addAttribute("searchType", cri.getSearchType());
+		rttr.addAttribute("keyword", cri.getKeyword());
+		
+		rttr.addFlashAttribute("msg","SUCCESS");
+		
+		return "redirect:/sboard/list";
+	}
 
 }
